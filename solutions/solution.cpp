e62cc9d849a59d088aab688bc395b5a2fe76c07c
Tanisha-0874 <tanishachekkala@gmail.com>
2025-05-25 19:47:53 +0530

    Update solution.cpp

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -6,6 +6,20 @@ using namespace std;
 class Solution {
 public:
     bool isValid(string s) {
-        
+        stack<char> st;
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                if (st.empty() ||
+                    (c == ')' && st.top() != '(') ||
+                    (c == '}' && st.top() != '{') ||
+                    (c == ']' && st.top() != '[')) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
     }
 };
