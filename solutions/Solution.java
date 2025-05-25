0521d6c5f542061010724a64c2f47b0a4b96e9c9
Tanisha-0874 <tanishachekkala@gmail.com>
2025-05-25 23:05:34 +0530

    Update Solution.java

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -6,7 +6,7 @@ public class Solution {
     public boolean isValid(String s) {
         Stack<Character> st=new Stack<Character>();
         for(char c:s.toCharArray()){
-            if(c=='(') st.push(')');
+            if(c=='(') st.push(')'); 
             else if(c=='{') st.push('}');
             else if(c=='[') st.push(']');
             else if(st.isEmpty() || st.pop()!=c)
